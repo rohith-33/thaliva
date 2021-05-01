f8e8f68c44db5c92d3dfe9d8714d6eb0006a34ea
rohith-33 <rohithmuthyala3@gmail.com>
2021-05-01 16:19:12 +0530

    hi friends

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var express=require('express');
 var app=express();
 var port=process.env.PORT || 3000;
 app.get('/',function(req,res){
-    res.send("hello thaliva");
+   // res.send("hello thaliva");
+   res.sendFile(__dirname+'/front end/html/index.html');
 });
 app.listen(port,function(){
    console.log("site running on http://localhost:"+port);
